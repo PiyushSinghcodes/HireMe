d24109e13c50ffc01c5e7b55c01f2c0bc9c8dcf1
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2024-11-12 19:04:16 +0530

    did

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -26,7 +26,7 @@ const AdminJobsTable = () => {
     return (
         <div>
             <Table>
-                <TableCaption>A list of your recent  posted jobs</TableCaption>
+                <TableCaption>A list of your recent posted jobs</TableCaption>
                 <TableHeader>
                     <TableRow>
                         <TableHead>Company Name</TableHead>
@@ -53,6 +53,10 @@ const AdminJobsTable = () => {
                                             <div onClick={()=> navigate(`/admin/jobs/${job._id}/applicants`)} className='flex items-center w-fit gap-2 cursor-pointer mt-2'>
                                                 <Eye className='w-4'/>
                                                 <span>Applicants</span>
+                                            </div>
+                                            <div onClick={()=> navigate(`/admin/companies/${job._id}`)} className='flex items-center gap-2 w-fit cursor-pointer'>
+                                                <Edit2 className='w-4' />
+                                                <span>Add Assesment</span>
                                             </div>
                                         </PopoverContent>
                                     </Popover>
