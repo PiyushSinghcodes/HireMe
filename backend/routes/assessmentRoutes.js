189cdb5720ad6b3e2acb7c736f94a2310e71a3c6
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2024-11-13 16:52:32 +0530

    ok2

--- backend/routes/assessmentRoutes.js
+++ backend/routes/assessmentRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import { createAssessment, updateAssessment, getAssessmentByJobId } from '../controllers/assessmentController';
+import { createAssessment, updateAssessment, getAssessmentByJobId } from '../controllers/assessmentController.js';
 
 // Route to create a new assessment
 router.post('/', createAssessment);
